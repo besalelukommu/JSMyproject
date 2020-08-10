8e73d17c3635f5e4ee34820b3bd42fd9313a14c9
besalelu kommu <kommu@besalelus-MacBook-Air.local>
2020-08-11 01:42:08 +0530

    Course completed

---
+++ JS Number Facts App/main.js
@@ -0,0 +1,37 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+let numberInput = document.querySelector('#numberInput');
+
+numberInput.addEventListener('input', getFetchFacts);
+
+// function getFactAjax(){
+    
+//     let number = numberInput.value;
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://numbersapi.com/'+number);
+//     xhr.onload = function(){
+//         if(this.status == 200 && number != ''){
+//             fact.style.display = 'block';
+//             factText.innerHTML = this.responseText;
+            
+//         }
+//     }
+
+//     xhr.send();
+// }
+
+function getFetchFacts(){
+    let number = numberInput.value;
+
+    fetch('http://numbersapi.com/'+number)
+    .then(response => response.text())
+    .then(data => {
+        if(number != ''){
+            fact.style.display = 'block';
+            factText.innerHTML = data; 
+        }
+    })
+    .catch(err => console.log(err));
+}
+
